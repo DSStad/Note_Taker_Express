c7c500c1f1ddf09e519cd2194497c78e9b02ee87
Derick Stadler <derickstadler@gmail.com>
2023-06-20 18:43:04 -0600

    created more routes and helper directory.

--- Develop/server.js
+++ Develop/server.js
@@ -2,15 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const PORT = process.env.PORT || 3001;
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
 
 app.use(express.static("public"));
 
+// path to index.html
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-
+// path to notes.html
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
